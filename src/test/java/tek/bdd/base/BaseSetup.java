ca1af2c05d1935e42d8f7425e104fe97088f1499
akowa <akowa344@gmail.com>
2024-07-30 19:40:45 -0400

    bdd-commit2

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,22 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class BaseSetup {
+    private static WebDriver driver;
+    public void setupBrowser(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    public void quitBrowser(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+}
